392e330fa67ec82df0e91facdd7df8c2db382c13
Joe Bravo <joepbravo@gmail.com>
2025-05-11 22:20:18 -0700

    Adding logic to load synthetic data to database with updated config file.

--- src/creating_synthetic_data.py
+++ src/creating_synthetic_data.py
@@ -11,9 +11,11 @@ import os
 import psycopg2 # To handle connection to postgresql
 import yaml # For configuration file
 # import matplotlib.pyplot as plt
-import numpy as np
-np.random.seed(1234)
+# import numpy as np
+# np.random.seed(1234)
 import pandas as pd
+from io import StringIO
+import time
 
 # Some custom helper functions
 from helper_funcs.pfp_helper_funcs import (
@@ -40,6 +42,11 @@ num_data_pts = 150
 start_date = '2023-12-01'
 end_date = '2023-12-31'
 
+# Part 3: Create dataframes and concatenate
+##################################################################################
+# Will call the functions and store them as dataframes, then join all three
+# dataframes.
+##################################################################################
 # Date and day dataframe
 df1 = date_day_df(
     start_date,
@@ -56,6 +63,76 @@ df3 = pd.concat(
     [df1, df2], 
     axis=1
 )
-print(df3)
+# print(df3)
+
+# Adding the txn_id which also works as a primary key (column).
+df3.insert(0, 'txn_id', range(len(df3)))
+
+# Will add the final amount column
+processed_df = df3
+processed_df['Amount'] = processed_df['Category'].map(amount)
+
+# Part 3: Open connection to database
+##################################################################################
+# Will use the configuration file to open a connection to Postgres and populate a
+# new table.
+##################################################################################
+# Get the directory of the current script
+script_dir = os.path.dirname(os.path.abspath(__file__))
+# Change the current working
+os.chdir(script_dir)
+# Read the config file
+with open("config.yaml", "r") as file:
+    config = yaml.safe_load(file)
+conn = psycopg2.connect(
+    f"host={config["server"]["host"]} \
+    dbname={config["synth_data"]["database"]} \
+    user={config["database"]["user"]} \
+    password={config["database"]["password"]} \
+    "
+)
+# To issue queries to the server
+cur = conn.cursor()
+# Execute commands on the postgres db
+cur.execute(
+    f"""CREATE TABLE IF NOT EXISTS {config["synth_data"]["tbl_name"]} (
+        txn_id SERIAL PRIMARY key
+        , date DATE NOT NULL
+        , day VARCHAR NOT NULL
+        , description VARCHAR NOT NULL
+        , category VARCHAR NOT NULL
+        , amount FLOAT NOT  NULL
+        );
+    """
+)
+# Retrieve csv and clean up the data
+# raw_data = pd.read_csv(config["directories"]["csv_file"])
+# raw_data['date'] = pd.to_datetime(raw_data['date']).dt.date
+
+start_time = time.time() # Get start time before insert
+
+sio = StringIO()
+processed_df.to_csv(sio, index=None, header=None)
+sio.seek(0)
+with conn.cursor() as c:
+    c.copy_expert(
+        sql=f"""COPY {config["synth_data"]["tbl_name"]} (
+                txn_id,
+                date,
+                day,
+                description,
+                category,
+                amount
+                ) FROM STDIN WITH CSV
+            """,
+        file=sio
+    )
+# Commit the changes
+conn.commit()
+end_time = time.time() # Get end time after insert
+total_time = end_time - start_time # Calculate the time difference
+print(f"Insert time: {total_time} seconds") # Print time
+#Close the connection
+conn.close()
 
 # End of "creating_synthetic_data.py"
